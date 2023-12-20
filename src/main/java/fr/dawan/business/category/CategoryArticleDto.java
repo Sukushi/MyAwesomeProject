531f951279d9d48d43b88df7b8e33285ef842c19
Sukushi <romain.chuche@gmail.com>
2023-12-20 09:48:08 +0100

    Fix : findDtoById for Category

--- src/main/java/fr/dawan/business/category/CategoryArticleDto.java
+++ src/main/java/fr/dawan/business/category/CategoryArticleDto.java
@@ -1,20 +1,18 @@
 package fr.dawan.business.category;
 
-import fr.dawan.business.article.ArticleDto;
+
+import fr.dawan.business.article.ArticleCategoryDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class CategoryDto {
+@AllArgsConstructor
+public class CategoryArticleDto {
 	
 	private long id;
 	private int version;
-	private String name;
-	private List<CategoryArticleDto> articles;
+	private String title;
 	
 }
