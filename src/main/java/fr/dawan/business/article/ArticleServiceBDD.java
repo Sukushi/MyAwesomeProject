40e7e26bbc7f1d880350cd54e3966de6dedb8726
Sukushi <romain.chuche@gmail.com>
2023-12-21 10:12:12 +0100

    Feat : here a log

--- src/main/java/fr/dawan/business/article/ArticleServiceBDD.java
+++ src/main/java/fr/dawan/business/article/ArticleServiceBDD.java
@@ -1,15 +1,18 @@
 package fr.dawan.business.article;
 
 import fr.dawan.business.generic.GenericServiceBDD;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Pageable;
 
 @Service
+@Slf4j // crée un logger pour la classe
 public class ArticleServiceBDD extends GenericServiceBDD<Article,ArticleRepository,ArticleDto,ArticleMapper> implements ArticleService {
 	
 	public ArticleServiceBDD(ArticleRepository repository, ArticleMapper mapper) {
 		super(repository, mapper);
+		hereALog();
 	}
 	
 	@Override
@@ -20,6 +23,10 @@ public class ArticleServiceBDD extends GenericServiceBDD<Article,ArticleReposito
 	@Override
 	public Page<ArticleDto> findByCategory_NameIgnoreCase(String name, Pageable pageable) {
 		return repository.findByCategory_NameIgnoreCase(name,pageable).map(mapper::toDto);
+	}
+	
+	public void hereALog() {
+		log.info("I'm here !");
 	}
 	
 }
